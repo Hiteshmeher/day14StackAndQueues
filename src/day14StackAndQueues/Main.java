9121e4410a60f3d4c7232ea9ca9592cdadb68de8
Hiteshmeher <meher.hitesh4@gmail.com>
2022-07-12 18:15:11 +0530

    Stack operation using linked list

---
+++ src/day14StackAndQueues/Main.java
@@ -0,0 +1,12 @@
+package day14StackAndQueues;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Linkedlist list = new Linkedlist();
+		list.push(70);
+		list.push(30);
+		list.push(56);
+		list.display();
+	}
+}
