bfc2291ecbd2d0ed5fd1c572f5be6bc672cdb74b
Hiteshmeher <meher.hitesh4@gmail.com>
2022-07-12 19:22:04 +0530

    added to create Queue of 56 30 70

--- src/day14StackAndQueues/Linkedlist.java
+++ src/day14StackAndQueues/Linkedlist.java
@@ -57,16 +57,15 @@ public class Linkedlist<T> {
 		/**
 		 * adding values
 		 */
-        Node newNode = new Node(data);
-        if (head == null){
-            head = newNode;
-            tail = newNode;
-        }
-        else {
-            newNode.next = head;
-            head = newNode;
-        }
-    }
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			newNode.next = head;
+			head = newNode;
+		}
+	}
 
 //	public void insertBetween( int data, int newNode) {
 //		Node newNode = new Node(data);
@@ -77,13 +76,14 @@ public class Linkedlist<T> {
 //		prevNode.next = newNode;
 //		newNode.next = tempNode;
 //	}
-	 public void pop() {
-	        head = head.next;
-	    }
+	public void pop() {
+		head = head.next;
+	}
+
 	public Object pop(int data) {
 		/**
-		 * finding second last node as temp
-		 * and temp.next means the last node defined as null
+		 * finding second last node as temp and temp.next means the last node defined as
+		 * null
 		 * 
 		 */
 		if (head == null) {
@@ -100,20 +100,19 @@ public class Linkedlist<T> {
 
 		return head;
 	}
-	
+
 	public int size() {
 		/**
 		 * size of the stack
 		 */
-        Node temp=head;
-        int count = 0;
-        while(temp!=null)
-        {
-            temp=temp.next;
-            count++;
-        }
-        return count;
-    }
+		Node temp = head;
+		int count = 0;
+		while (temp != null) {
+			temp = temp.next;
+			count++;
+		}
+		return count;
+	}
 
 	public Node search(int data) {
 		/**
@@ -128,4 +127,20 @@ public class Linkedlist<T> {
 		}
 		return null;
 	}
+
+	public void remove(int position) {
+		Node temp = head;
+		if (head == null)
+			return;
+		if (position == 0) {
+			head = temp.next;
+			return;
+		}
+		for (int j = 0; temp != null && j < position - 1; j++) {
+			temp = temp.next;
+			Node next = temp.next.next;
+			temp.next = next;
+		}
+	}
+
 }
---
+++ src/day14StackAndQueues/Queues.java
@@ -0,0 +1,12 @@
+package day14StackAndQueues;
+
+public class Queues {
+
+	public static void main(String[] args) {
+		Linkedlist list = new Linkedlist();
+		list.push(56); // here push performs enque operation
+		list.push(30);
+		list.push(70);
+		list.display();
+	}
+}
