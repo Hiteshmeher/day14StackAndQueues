74f8e467515d5e9b2fcbb4881850278bde4a5059
Hiteshmeher <meher.hitesh4@gmail.com>
2022-07-13 15:46:57 +0530

    added Dequeue operation from beginning

--- src/day14StackAndQueues/Queues.java
+++ src/day14StackAndQueues/Queues.java
@@ -4,9 +4,16 @@ public class Queues {
 
 	public static void main(String[] args) {
 		Linkedlist list = new Linkedlist();
-		list.push(56); // here push performs enque operation
-		list.push(30);
-		list.push(70);
-		list.display();
-	}
+		 list.push(70);
+	        list.push(30);
+	        list.push(56);
+	        list.display();
+	        System.out.println("Node size is " +list.size());
+	        while(list.size() != 0){
+	            list.pop();
+	        }
+	        list.display();
+	        System.out.println("\nThe Stack is empty....");
+
+	    }
 }
